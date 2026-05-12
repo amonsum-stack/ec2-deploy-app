cbe2cd09f634d357e5b0300fccda449bcfae0f2e
amonsum <amon.sum@gmail.com>
2026-05-12 10:34:44 +0200

    removed variables

--- ec2-deploy-app/terraform.tfvars
+++ ec2-deploy-app/terraform.tfvars
@@ -1,5 +1,3 @@
-
-
 vpc_cidr = "10.0.0.0/16"
 
 vpc_name = "test-vpc"
--- ec2-deploy-app/variables.tf
+++ ec2-deploy-app/variables.tf
@@ -48,20 +48,12 @@ variable "ec2_security_group_description" {
   type        = string
 }
 
-variable "ami_id" {
-  description = "The AMI ID for the EC2 instances"
-  type        = string
-}
 
 variable "instance_type" {
   description = "The instance type for the EC2 instances"
   type        = string
 }
 
-variable "key_name" {
-  description = "The name of the SSH key pair"
-  type        = string
-}
 
 variable "lb_name" {
   description = "The name of the load balancer"
